b2f58c719e6d77256eb173d328542b9abc3a4277
Halil <yilmazhalilibr@gmail.com>
2024-05-29 14:51:23 +0300

    Init project and created 2 class

---
+++ Source/Unreal_SpatialCpp/Public/SpatialInventory/InventorySubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "InventorySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_SPATIALCPP_API UInventorySubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
