ccded915b96ae70ac769b6e9b81b3d983c8becde
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2024-06-22 10:25:23 +0300

    Bug bug fixed 46:00 worked

--- Source/Unreal_SpatialCpp/Private/InventoryWidget.cpp
+++ Source/Unreal_SpatialCpp/Private/InventoryWidget.cpp
@@ -14,91 +14,85 @@
 
 void UInventoryWidget::NativeConstruct()
 {
-    Super::NativeConstruct();
+	Super::NativeConstruct();
 
-    InventorySubsystem = GetGameInstance()->GetSubsystem<UInventorySubsystem>();
+	InventorySubsystem = GetGameInstance()->GetSubsystem<UInventorySubsystem>();
 
-    if (InventorySubsystem)
-    {
-        if (InventoryGrid)
-        {
-            UE_LOG(LogTemp, Warning, TEXT("InventoryGrid found."));
-            InventoryGrid->InitializeGrids(this);
-        }
-        else
-        {
-            UE_LOG(LogTemp, Error, TEXT("InventoryGrid is not bound."));
-        }
+	if (InventorySubsystem)
+	{
+		if (InventoryGrid)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("InventoryGrid found."));
+			InventoryGrid->InitializeGrids(this);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("InventoryGrid is not bound."));
+		}
 
-        // BackgroundBorder'ı valid et ve sonra mouse button down eventini bağla
-        if (BackgroundBorder)
-        {
-            BackgroundBorder->OnMouseButtonDownEvent.BindUFunction(this, FName("OnBackgroundBorderMouseDown"));
-        }
-        else
-        {
-            UE_LOG(LogTemp, Error, TEXT("BackgroundBorder is not bound."));
-        }
-    }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("InventorySubsystem is not found."));
-    }
+		// BackgroundBorder'ı valid et ve sonra mouse button down eventini bağla
+		if (BackgroundBorder)
+		{
+			BackgroundBorder->OnMouseButtonDownEvent.BindUFunction(this, FName("OnBackgroundBorderMouseDown"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("BackgroundBorder is not bound."));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("InventorySubsystem is not found."));
+	}
 }
 
 FReply UInventoryWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
 {
-    UE_LOG(LogTemp, Warning, TEXT("NativeOnMouseButtonDown"));
+	UE_LOG(LogTemp, Warning, TEXT("NativeOnMouseButtonDown"));
 
-    if (BackgroundBorder && InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("NativeOnMouseButtonDown LeftMouseButton"));
-        return FReply::Handled();
-    }
+	if (BackgroundBorder && InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NativeOnMouseButtonDown LeftMouseButton"));
+		return FReply::Handled();
+	}
 
-    return FReply::Unhandled();
+	return FReply::Unhandled();
 }
 
 bool UInventoryWidget::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
 {
-    // Payload'ı AItemObject'e cast et
-    AItemObject* ItemObject = Cast<AItemObject>(InOperation->Payload);
-    if (ItemObject)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("NativeOnDrop - TRUE"));
+	AItemObject* ItemObject = Cast<AItemObject>(InOperation->Payload);
 
-        APlayerController* PlayerController = GetOwningPlayer();
-        if (PlayerController)
-        {
-            AActor* _player = PlayerController->GetPawn();
-            if (_player && InventorySubsystem)
-            {
-                InventorySubsystem->SpawnItemFromActor(ItemObject, _player, true);
-                InventorySubsystem->RemoveItem(ItemObject);
-                return true;
-            }
-            else
-            {
-                UE_LOG(LogTemp, Error, TEXT("Player or InventorySubsystem is null."));
-            }
-        }
-        else
-        {
-            UE_LOG(LogTemp, Error, TEXT("PlayerController is null."));
-        }
-    }
+	// Payload'ı AItemObject'e cast et
+	if (ItemObject)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NativeOnDrop - TRUE"));
 
-    // Eşya taşınamazsa, envanterde ilk bulunduğu yerde kalmasını sağla
-    if (InventorySubsystem && ItemObject)
-    {
-        bool bIsSuccess;
-        InventorySubsystem->TryAddItem(ItemObject, bIsSuccess);
-        if (!bIsSuccess)
-        {
-            UE_LOG(LogTemp, Warning, TEXT("Item could not be re-added to the inventory."));
-        }
-    }
+		APlayerController* PlayerController = GetOwningPlayer();
+		if (PlayerController)
+		{
+			AActor* _player = PlayerController->GetPawn();
+			if (_player && InventorySubsystem)
+			{
+				InventorySubsystem->SpawnItemFromActor(ItemObject, _player, true);
+				InventorySubsystem->RemoveItem(ItemObject);
+				return true;
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("Player or InventorySubsystem is null."));
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("PlayerController is null."));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("ItemObject is null."));
+	}
 
-    UE_LOG(LogTemp, Warning, TEXT("NativeOnDrop - FALSE"));
-    return false;
+	UE_LOG(LogTemp, Warning, TEXT("NativeOnDrop - FALSE"));
+	return false;
 }
